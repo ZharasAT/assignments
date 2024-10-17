d6ff8390a47fe63fd297d3e33fdb19b25b8d6b9e
Zharas <zharas.tuyanov@gmail.com>
2024-10-17 15:05:40 +0500

    Exercise 5: is-vowel

---
+++ assignment6/5.py
@@ -0,0 +1,9 @@
+def Vowel_or_Consonant(x):
+    if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u':
+        print(x + ' is a vowel')
+    else:
+        print(x + ' is not vowel')
+
+Vowel_or_Consonant('e')
+Vowel_or_Consonant('g')
+Vowel_or_Consonant('1')
