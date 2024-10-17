12bb90df94adb78d11bd0cbae60dbfdecf8b1bb6
Zharas <zharas.tuyanov@gmail.com>
2024-10-17 15:51:59 +0500

    Exercise 9: is-even

---
+++ assignment6/9.py
@@ -0,0 +1,8 @@
+def is_number_even(x):
+    if x % 2 == 0:
+        print("The " + str(x) + " is even")
+    else:
+        print("The " + str(x) + " isn't even")
+
+is_number_even(1001)
+is_number_even(500)
