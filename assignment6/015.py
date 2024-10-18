e733cb80c2542091e1aef2e62fde7a5e30558714
Zharas <zharas.tuyanov@gmail.com>
2024-10-18 15:17:43 +0500

    Exercise 15: calculate-quotient-remainder (Upd)

--- assignment6/015.py
+++ assignment6/015.py
@@ -1,6 +1,19 @@
+# a = 23
+# b = 7
+#
+# q, r = divmod(a, b)
+# print("Quotient: ", q)
+# print("Remainder: ", r)
+
 a = 23
 b = 7
 
-q, r = divmod(a, b)
-print("Quotient: ", q)
-print("Remainder: ", r)
+quotient = a // b
+
+if a / b == a // b:
+    print("Quotient: ", quotient)
+    print("Remainder: without remainder")
+
+elif a / b > a // b:
+    print("Quotient: ", quotient)
+    print("Remainder: ", a - quotient * b)
