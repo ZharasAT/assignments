0cdb600095f10d0be8a3b62399dd94d5124b1fd5
Zharas <zharas.tuyanov@gmail.com>
2025-01-10 22:12:01 +0500

    4. fastapi-meaning-life_Resolved

---
+++ HW_WSGI_FastAPI/fastapi_meaning_life.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, nfactorial!"}
+
+@app.post("/meaning-of-life")
+def meaning_of_life():
+    def meaning_of_life():
+        return {"meaning": "42"}
