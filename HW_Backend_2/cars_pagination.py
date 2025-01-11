bcd14b37afb4e1b7e4d391550bf69e8f9c2fdf68
Zharas <zharas.tuyanov@gmail.com>
2025-01-11 22:13:40 +0500

    users-all resolved

--- HW_Backend_2/cars_pagination.py
+++ HW_Backend_2/cars_pagination.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
@@ -6,6 +9,38 @@ cars = [
     {"id": i, "brand": f"Brand-{i}", "model": f"Model-{i}", "year": 2000 + i % 20}
     for i in range(1, 101)
 ]
+
+class User(BaseModel):
+    id: int
+    email: str
+    first_name: str
+    last_name: str
+    username: str
+
+users = [
+    User(
+        id=1,
+        email="test@test.com",
+        first_name="Aibek",
+        last_name="Bekturov",
+        username="deadly_knight95"
+    ),
+User(
+        id=2,
+        email="marzhan@example.com",
+        first_name="Marzhan",
+        last_name="Pak",
+        username="dark_angel",
+    ),
+    User(
+        id=3,
+        email="yermek@nfactorial.com",
+        first_name="Yermek",
+        last_name="Nurash",
+        username="oiboy97",
+    ),
+]
+
 
 @app.get("/cars")
 def get_cars(page: int = 1, limit: int = 10):
@@ -20,3 +55,41 @@ def get_car_by_id(car_id: int):
     if car is None:
         raise HTTPException(status_code=404, detail="Not found")
     return car
+
+@app.get("/users", response_class=HTMLResponse)
+def get_users():
+    html_content = """
+    <html>
+        <head>
+            <title>Users List</title>
+        </head>
+        <body>
+            <h1>Users</h1>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+    """
+
+    for user in users:
+        user_row = f"""
+        <tr>
+            <td>{user.username}</td>
+            <td>
+                <a href="/users/{user.id}">{user.first_name} {user.last_name}</a>
+            </td>
+        </tr>
+        """
+        html_content += user_row
+
+    html_content += """
+                </tbody>
+            </table>
+        </body>
+    </html>
+    """
+    return html_content
