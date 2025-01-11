d57f225fed7b2188563d2deb849227f74656e8c2
Zharas <zharas.tuyanov@gmail.com>
2025-01-11 21:22:30 +0500

    cars-id resolved

--- HW_Backend_2/cars_pagination.py
+++ HW_Backend_2/cars_pagination.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -12,3 +12,11 @@ def get_cars(page: int = 1, limit: int = 10):
     start = (page - 1) * limit
     end = start + limit
     return cars[start:end]
+
+@app.get("/cars/{car_id}")
+def get_car_by_id(car_id: int):
+    car = next((car for car in cars if car["id"] == car_id), None)
+
+    if car is None:
+        raise HTTPException(status_code=404, detail="Not found")
+    return car
